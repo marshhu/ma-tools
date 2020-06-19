393a77bddf0fcf5a257160e8b45090d2ce51df45
v_zezaohu <v_zezaohu@tencent.com>
2020-06-19 17:29:55 +0800

    html转pdf

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ma-tools
 
 go 1.14
+
+require github.com/SebastiaanKlippert/go-wkhtmltopdf v1.5.0
--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "ma-tools/htmltopdf"
 
+func main() {
+	htmltopdf.ExampleNewPDFGenerator()
 }
