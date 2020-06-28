05bdd996ba5f80dbfcb53f31d0491f49564d93f7
v_zezaohu <v_zezaohu@tencent.com>
2020-06-28 17:12:13 +0800

    修改

--- simple_crawler/crawler_test.go
+++ simple_crawler/crawler_test.go
@@ -2,8 +2,6 @@ package simple_crawler
 
 import (
 	"fmt"
-	"net/http"
-	"regexp"
 	"sync"
 	"testing"
 	"time"
@@ -11,17 +9,25 @@ import (
 
 func Test_CrawlerPlatform(t *testing.T) {
 	list := []JobInfo{
-		{PlatformName: "今日头条", Url: "https://www.toutiao.com/a6842087368221000200"},
-		{PlatformName: "搜狐网", Url: "https://www.sohu.com/a/404042582_496421?spm=smpc.mil-home.feed.1.1593052479609Cw5rk4s"},
-		{PlatformName: "网易新闻", Url: "https://news.163.com/20/0625/09/FFV6B8VQ0001899O.html"},
-		{PlatformName: "新浪财经", Url: "https://finance.sina.com.cn/roll/2020-06-25/doc-iirczymk8863563.shtml"},
-		{PlatformName: "新浪微博", Url: "https://s.weibo.com/weibo?q=%23%E4%BF%84%E7%BD%97%E6%96%AF%E7%BA%A2%E5%9C%BA%E9%98%85%E5%85%B5%E5%BC%8F%E7%9B%B4%E6%92%AD%23&from=default"},
-		{PlatformName: "凤凰新闻", Url: "https://news.ifeng.com/c/7iyXEy1Ptaf"},
-		{PlatformName: "东方财富网", Url: "http://finance.eastmoney.com/a/202006251533566237.html"},
-		{PlatformName: "海峡网", Url: "http://www.hxnews.com/news/fj/fz/202006/23/1907292.shtml"},
-		{PlatformName: "河北新闻网", Url: "http://hebei.hebnews.cn/2020-06/25/content_7959208.htm"},
-		{PlatformName: "It之家", Url: "https://www.ithome.com/0/494/604.htm"},
-		{PlatformName: "百度", Url: "https://mbd.baidu.com/newspage/data/landingsuper?context=%7B%22nid%22%3A%22news_9872723525150545336%22%7D&n_type=0&p_from=1"},
+		//{PlatformName: "今日头条", Url: "https://www.toutiao.com/a6842087368221000200"},
+		//{PlatformName: "搜狐网", Url: "https://www.sohu.com/a/404042582_496421?spm=smpc.mil-home.feed.1.1593052479609Cw5rk4s"},
+		//{PlatformName: "网易新闻", Url: "https://news.163.com/20/0625/09/FFV6B8VQ0001899O.html"},
+		//{PlatformName: "新浪财经", Url: "https://finance.sina.com.cn/roll/2020-06-25/doc-iirczymk8863563.shtml"},
+		//{PlatformName: "新浪微博", Url: "https://s.weibo.com/weibo?q=%23%E4%BF%84%E7%BD%97%E6%96%AF%E7%BA%A2%E5%9C%BA%E9%98%85%E5%85%B5%E5%BC%8F%E7%9B%B4%E6%92%AD%23&from=default"},
+		//{PlatformName: "凤凰新闻", Url: "https://news.ifeng.com/c/7iyXEy1Ptaf"},
+		//{PlatformName: "东方财富网", Url: "http://finance.eastmoney.com/a/202006251533566237.html"},
+		//{PlatformName: "海峡网", Url: "http://www.hxnews.com/news/fj/fz/202006/23/1907292.shtml"},
+		//{PlatformName: "河北新闻网", Url: "http://hebei.hebnews.cn/2020-06/25/content_7959208.htm"},
+		//{PlatformName: "It之家", Url: "https://www.ithome.com/0/494/604.htm"},
+		//{PlatformName: "百度", Url: "https://mbd.baidu.com/newspage/data/landingsuper?context=%7B%22nid%22%3A%22news_9872723525150545336%22%7D&n_type=0&p_from=1"},
+		{PlatformName: "今日头条", Url: "https://www.toutiao.com/a6843224848617964036"},
+		{PlatformName: "今日头条", Url: "https://www.toutiao.com/a6843224848617964038"},
+		{PlatformName: "今日头条", Url: "https://www.toutiao.com/a6843224848617964039"},
+		{PlatformName: "今日头条", Url: "https://www.toutiao.com/a6843224848617964041"},
+		{PlatformName: "今日头条", Url: "https://www.toutiao.com/a6843224848617964042"},
+		{PlatformName: "今日头条", Url: "https://www.toutiao.com/a6843224848617964043"},
+		{PlatformName: "今日头条", Url: "https://www.toutiao.com/a6843224848617964044"},
+		{PlatformName: "今日头条", Url: "https://www.toutiao.com/a6843224848617964035"},
 	}
 
 	startTime := time.Now()
@@ -33,17 +39,21 @@ func Test_CrawlerPlatform(t *testing.T) {
 func Benchmark_CrawlerPlatform(b *testing.B) {
 	b.StopTimer()
 	list := []JobInfo{
-		{PlatformName: "今日头条", Url: "https://www.toutiao.com/a6842087368221000200"},
-		{PlatformName: "搜狐网", Url: "https://www.sohu.com/a/404042582_496421?spm=smpc.mil-home.feed.1.1593052479609Cw5rk4s"},
-		{PlatformName: "网易新闻", Url: "https://news.163.com/20/0625/09/FFV6B8VQ0001899O.html"},
-		{PlatformName: "新浪财经", Url: "https://finance.sina.com.cn/roll/2020-06-25/doc-iirczymk8863563.shtml"},
-		{PlatformName: "新浪微博", Url: "https://s.weibo.com/weibo?q=%23%E4%BF%84%E7%BD%97%E6%96%AF%E7%BA%A2%E5%9C%BA%E9%98%85%E5%85%B5%E5%BC%8F%E7%9B%B4%E6%92%AD%23&from=default"},
-		{PlatformName: "凤凰新闻", Url: "https://news.ifeng.com/c/7iyXEy1Ptaf"},
-		{PlatformName: "东方财富网", Url: "http://finance.eastmoney.com/a/202006251533566237.html"},
-		{PlatformName: "海峡网", Url: "http://www.hxnews.com/news/fj/fz/202006/23/1907292.shtml"},
-		{PlatformName: "河北新闻网", Url: "http://hebei.hebnews.cn/2020-06/25/content_7959208.htm"},
-		{PlatformName: "It之家", Url: "https://www.ithome.com/0/494/604.htm"},
-		{PlatformName: "百度", Url: "https://mbd.baidu.com/newspage/data/landingsuper?context=%7B%22nid%22%3A%22news_9872723525150545336%22%7D&n_type=0&p_from=1"},
+		//{PlatformName: "今日头条", Url: "https://www.toutiao.com/a6842087368221000200"},
+		//{PlatformName: "搜狐网", Url: "https://www.sohu.com/a/404042582_496421?spm=smpc.mil-home.feed.1.1593052479609Cw5rk4s"},
+		//{PlatformName: "网易新闻", Url: "https://news.163.com/20/0625/09/FFV6B8VQ0001899O.html"},
+		//{PlatformName: "新浪财经", Url: "https://finance.sina.com.cn/roll/2020-06-25/doc-iirczymk8863563.shtml"},
+		//{PlatformName: "新浪微博", Url: "https://s.weibo.com/weibo?q=%23%E4%BF%84%E7%BD%97%E6%96%AF%E7%BA%A2%E5%9C%BA%E9%98%85%E5%85%B5%E5%BC%8F%E7%9B%B4%E6%92%AD%23&from=default"},
+		//{PlatformName: "凤凰新闻", Url: "https://news.ifeng.com/c/7iyXEy1Ptaf"},
+		//{PlatformName: "东方财富网", Url: "http://finance.eastmoney.com/a/202006251533566237.html"},
+		//{PlatformName: "海峡网", Url: "http://www.hxnews.com/news/fj/fz/202006/23/1907292.shtml"},
+		//{PlatformName: "河北新闻网", Url: "http://hebei.hebnews.cn/2020-06/25/content_7959208.htm"},
+		//{PlatformName: "It之家", Url: "https://www.ithome.com/0/494/604.htm"},
+		//{PlatformName: "百度", Url: "https://mbd.baidu.com/newspage/data/landingsuper?context=%7B%22nid%22%3A%22news_9872723525150545336%22%7D&n_type=0&p_from=1"},
+		{PlatformName: "今日头条", Url: "https://www.toutiao.com/a6843224848617964036"},
+		{PlatformName: "今日头条", Url: "https://www.toutiao.com/a6843224848617964038"},
+		{PlatformName: "今日头条", Url: "https://www.toutiao.com/a6843224848617964039"},
+		{PlatformName: "今日头条", Url: "https://www.toutiao.com/a6843224848617964035"},
 	}
 	wg := &sync.WaitGroup{}
 	b.StartTimer()
@@ -58,33 +68,34 @@ func Benchmark_CrawlerPlatform(b *testing.B) {
 }
 
 func printHandleResult(workResult *WorkResult) {
-	urlContentMap := make(map[string]string)
-	urlContentMap["https://www.toutiao.com/a6842087368221000200"] = "新冠病毒抗体研究获重大突破"
-	urlContentMap["https://www.sohu.com/a/404042582_496421?spm=smpc.mil-home.feed.1.1593052479609Cw5rk4s"] = "美国疫情依然汹涌"
-	urlContentMap["https://news.163.com/20/0625/09/FFV6B8VQ0001899O.html"] = "美国医院床位告急"
-	urlContentMap["https://finance.sina.com.cn/roll/2020-06-25/doc-iirczymk8863563.shtml"] = "国美电器创始人黄光裕获假释"
-	urlContentMap["https://s.weibo.com/weibo?q=%23%E4%BF%84%E7%BD%97%E6%96%AF%E7%BA%A2%E5%9C%BA%E9%98%85%E5%85%B5%E5%BC%8F%E7%9B%B4%E6%92%AD%23&from=default"] = "俄罗斯红场阅兵式直播"
-	urlContentMap["https://news.ifeng.com/c/7iyXEy1Ptaf"] = "国安部原副部长马建被判无期"
-	urlContentMap["http://finance.eastmoney.com/a/202006251533566237.html"] = `券商股进入估值“攻势周期”`
-	urlContentMap["http://www.hxnews.com/news/fj/fz/202006/23/1907292.shtml"] = "保险合同非本人签名后续"
-	urlContentMap["http://hebei.hebnews.cn/2020-06/25/content_7959208.htm"] = "新冠病毒抗体研究获重大突破"
-	urlContentMap["http://hebei.hebnews.cn/2020-06/25/content_7959208.htm"] = "报告新型冠状病毒肺炎"
-	urlContentMap["https://www.ithome.com/0/494/604.htm"] = `苹果 iOS/iPadOS 14“新面孔”`
-	urlContentMap["https://mbd.baidu.com/newspage/data/landingsuper?context=%7B%22nid%22%3A%22news_9872723525150545336%22%7D&n_type=0&p_from=1"] = "CIM新基建，CityBase的定位与腾讯的版图"
+	//urlContentMap := make(map[string]string)
+	//urlContentMap["https://www.toutiao.com/a6842087368221000200"] = "新冠病毒抗体研究获重大突破"
+	//urlContentMap["https://www.sohu.com/a/404042582_496421?spm=smpc.mil-home.feed.1.1593052479609Cw5rk4s"] = "美国疫情依然汹涌"
+	//urlContentMap["https://news.163.com/20/0625/09/FFV6B8VQ0001899O.html"] = "美国医院床位告急"
+	//urlContentMap["https://finance.sina.com.cn/roll/2020-06-25/doc-iirczymk8863563.shtml"] = "国美电器创始人黄光裕获假释"
+	//urlContentMap["https://s.weibo.com/weibo?q=%23%E4%BF%84%E7%BD%97%E6%96%AF%E7%BA%A2%E5%9C%BA%E9%98%85%E5%85%B5%E5%BC%8F%E7%9B%B4%E6%92%AD%23&from=default"] = "俄罗斯红场阅兵式直播"
+	//urlContentMap["https://news.ifeng.com/c/7iyXEy1Ptaf"] = "国安部原副部长马建被判无期"
+	//urlContentMap["http://finance.eastmoney.com/a/202006251533566237.html"] = `券商股进入估值“攻势周期”`
+	//urlContentMap["http://www.hxnews.com/news/fj/fz/202006/23/1907292.shtml"] = "保险合同非本人签名后续"
+	//urlContentMap["http://hebei.hebnews.cn/2020-06/25/content_7959208.htm"] = "新冠病毒抗体研究获重大突破"
+	//urlContentMap["http://hebei.hebnews.cn/2020-06/25/content_7959208.htm"] = "报告新型冠状病毒肺炎"
+	//urlContentMap["https://www.ithome.com/0/494/604.htm"] = `苹果 iOS/iPadOS 14“新面孔”`
+	//urlContentMap["https://mbd.baidu.com/newspage/data/landingsuper?context=%7B%22nid%22%3A%22news_9872723525150545336%22%7D&n_type=0&p_from=1"] = "CIM新基建，CityBase的定位与腾讯的版图"
+	//
+	//if workResult.HttpStatus != http.StatusOK {
+	//	fmt.Printf("%s平台，http状态为:%d,查找结果为：%v\n", workResult.Job.PlatformName, workResult.HttpStatus, false)
+	//	return
+	//}
+	//
+	////查找内容
+	//re := regexp.MustCompile(urlContentMap[workResult.Job.Url])
+	//findStr := re.FindString(string(workResult.Content))
+	//if len(findStr) > 0 {
+	//	fmt.Printf("%s平台，http状态为:%d,查找结果为：%v\n", workResult.Job.PlatformName, workResult.HttpStatus, true)
+	//	return
+	//}
 
-	if workResult.HttpStatus != http.StatusOK {
-		fmt.Printf("%s平台，http状态为:%d,查找结果为：%v\n", workResult.Job.PlatformName, workResult.HttpStatus, false)
-		return
-	}
-
-	//查找内容
-	re := regexp.MustCompile(urlContentMap[workResult.Job.Url])
-	findStr := re.FindString(string(workResult.Content))
-	if len(findStr) > 0 {
-		fmt.Printf("%s平台，http状态为:%d,查找结果为：%v\n", workResult.Job.PlatformName, workResult.HttpStatus, true)
-		return
-	}
-
-	fmt.Printf("%s平台，http状态为:%d,查找结果为：%v\n", workResult.Job.PlatformName, workResult.HttpStatus, false)
+	//fmt.Printf("%s平台，http状态为:%d,查找结果为：%v\n", workResult.Job.PlatformName, workResult.HttpStatus, false)
+	fmt.Printf("%s平台，http状态为:%d\n", workResult.Job.PlatformName, workResult.HttpStatus)
 	return
 }
--- simple_crawler/fetcher.go
+++ simple_crawler/fetcher.go
@@ -3,6 +3,7 @@ package simple_crawler
 import (
 	"bufio"
 	"errors"
+	"fmt"
 	"golang.org/x/net/html/charset"
 	"golang.org/x/text/encoding"
 	"golang.org/x/text/encoding/simplifiedchinese"
@@ -24,26 +25,17 @@ func Fetcher(url string, timeout int) (httpStatus int, body []byte, err error) {
 	req, _ := http.NewRequest("GET", url, nil)
 	userAgent := "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
 	req.Header.Add("User-Agent", userAgent)
+
+	//今日头条
+	if strings.Contains(url, "www.toutiao.com") {
+		cookieValue := "ttcid=a2067f315cdc49e09e94b1d0d81a3e9e38; SLARDAR_WEB_ID=d0ce9243-f932-4a9f-9ac9-60c42a250863; csrftoken=dc2f078f05af27daf0631e6184b2e4a8; WEATHER_CITY=%E5%8C%97%E4%BA%AC; sso_auth_status=1c7a287e4a7fcc377e31a39020a817e8; sso_uid_tt=e94c80692a76c0923a68c822dedf9c48; sso_uid_tt_ss=e94c80692a76c0923a68c822dedf9c48; toutiao_sso_user=efb6d8b362f877c79705f4a38ad9cce9; toutiao_sso_user_ss=efb6d8b362f877c79705f4a38ad9cce9; passport_auth_status=76bfa72c3d5af1c304263e6ae256c368%2C1f404406cf49209f866819dd3fe4f3f0; sid_guard=3041369e2d9012755f74021a5ed45b5b%7C1593145039%7C5184000%7CTue%2C+25-Aug-2020+04%3A17%3A19+GMT; uid_tt=d87c69248d57fa92f3a61f20351849fb; uid_tt_ss=d87c69248d57fa92f3a61f20351849fb; sid_tt=3041369e2d9012755f74021a5ed45b5b; sessionid=3041369e2d9012755f74021a5ed45b5b; sessionid_ss=3041369e2d9012755f74021a5ed45b5b; __utmz=24953151.1593162227.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); tt_webid=6843277474432992782; s_v_web_id=verify_kbyoeexd_hTsS4wcl_Pk5X_4PSn_9Qjn_vkSgGiStj39k; tt_webid=6843277474432992782; tt_scid=7m-U95sGkMlHSVaGOkeKywTxFj5jLpIn7EGJUamPRQEWq1C7kWsd-hfGuJx07G9g6b5f; __utmc=24953151; __utma=24953151.1722430384.1593162227.1593326605.1593332416.3; __utmb=24953151.3.10.1593332416;" +
+			fmt.Sprintf(" __ac_nonce=%s; __ac_signature=%s;", "05ef85a610096fa21182b", "_02B4Z6wo00f01i1EgNwAAIBBNBpm6FfcxzItQIRAANW2oCm8GD5cR4IHTVcDfp.VuRl.Z3r5r3jdx5fXLm21e7PJ6yH5B11olUkMfITzsfjVwJcDM8gPv4t3hu0s3ZSojQCPBziiG9D-zWdm56")
+		req.Header.Add("cookie", cookieValue)
+	}
 	resp, error := client.Do(req)
 	if error != nil || resp == nil {
 		return 0, nil, errors.New("获取失败")
 	}
-
-	////今日头条
-	//if strings.Contains(url, "www.toutiao.com") {
-	//	cookies := resp.Cookies()
-	//	if len(cookies) >= 0 {
-	//		var acNonce string
-	//		for _, cookie := range cookies {
-	//			if cookie.Name == "__ac_nonce" {
-	//				acNonce = cookies[0].Value
-	//				break
-	//			}
-	//		}
-	//		fmt.Println(acNonce)
-	//	}
-	//	fmt.Println(cookies)
-	//}
 
 	bodyReader := bufio.NewReader(resp.Body)
 	defer resp.Body.Close()
